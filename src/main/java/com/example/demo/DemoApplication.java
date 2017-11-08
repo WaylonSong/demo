d75cf67c211a919b13031c80cb1521e500da91d5
WaylonFocus <songwenlue@gmail.com>
2017-11-08 15:02:13 +0800

    update

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import com.cputech.modules.usermsg.annotation.EnableUserMsg;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
@@ -14,14 +15,18 @@ import org.springframework.web.bind.annotation.RestController;
 @EntityScan("com.example.demo.model")
 @EnableJpaRepositories("com.example.demo.repository")
 @ComponentScan("com.example.demo.config")
+@EnableAutoConfiguration(exclude = {
+		org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration.class
+})
 @EnableUserMsg
+
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@RequestMapping("/hello")
+	@RequestMapping("/")
 	public String index(){
 		return "Hello Demo";
 	}
--- src/main/java/com/example/demo/repository/PersonRepository.java
+++ src/main/java/com/example/demo/repository/PersonRepository.java
@@ -8,12 +8,11 @@ import com.example.demo.model.Person;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 @CrossOrigin
-@PreAuthorize("hasRole('ROLE_ADMIN')")
+//@PreAuthorize("hasRole('ROLE_ADMIN')")
 @RepositoryRestResource
 public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
     List<Person> findByLastName(@Param("name") String name);
