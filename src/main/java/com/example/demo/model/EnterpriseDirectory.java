aa35618243752f105762265ee9d8fedb773e5367
huangsongkai <296731066@qq.com>
2017-11-10 11:29:44 +0800

    2017年11月10日11:29:38

---
+++ src/main/java/com/example/demo/model/EnterpriseDirectory.java
@@ -0,0 +1,63 @@
+package com.example.demo.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import java.util.Date;
+
+/**
+ * Created by huang on 2017/11/10.
+ */
+@Entity
+@Data
+public class EnterpriseDirectory {
+    /**
+     * 企业名称
+     */
+    String enterpriseName;
+    /**
+     * 经营范围
+     */
+    String businessScope;
+    /**
+     * 企业地址
+     */
+    String enterpriseaddress;
+    /**
+     * 成立时间
+     */
+    Date establishmentime;
+    /**
+     * 联系人
+     */
+    String contacts;
+    /**
+     * 联系方式
+     */
+    String phone;
+    /**
+     * 占地面积
+     */
+    String areaCovered;
+    /**
+     * 规模
+     */
+    String scale;
+    /**
+     * 总投资
+     */
+    Double totalInvestment;
+    /**
+     * 在职人数
+     */
+    Integer incumbency;
+    /**
+     * 种植面积
+     */
+    String plantingArea;
+    /**
+     * 种植作物
+     */
+    String raiseCrops;
+
+}
---
+++ src/main/java/com/example/demo/repository/EnterpriseDirectoryRepository.java
@@ -0,0 +1,17 @@
+package com.example.demo.repository;
+
+/**
+ * Created by song on 2017/10/23.
+ */
+
+import com.example.demo.model.EnterpriseDirectory;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin
+@RepositoryRestResource
+public interface EnterpriseDirectoryRepository extends PagingAndSortingRepository<EnterpriseDirectory, Long> {
+
+    void deleteAll();
+}
