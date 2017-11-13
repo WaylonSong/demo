1b08219876de095e7eb9bc6db214ca7da374c024
huangsongkai <296731066@qq.com>
2017-11-13 16:57:30 +0800

    2017年11月13日16:57:23

--- src/main/java/com/example/demo/controller/RestUploadController.java
+++ src/main/java/com/example/demo/controller/RestUploadController.java
@@ -6,59 +6,81 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ClassUtils;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@CrossOrigin
 public class RestUploadController {
+    class State{
+        public String state;
+        public String url;
 
+        public String getState() {
+            return state;
+        }
+
+        public void setState(String state) {
+            this.state = state;
+        }
+
+        public String getUrl() {
+            return url;
+        }
+
+        public void setUrl(String url) {
+           this.url = url;
+        }
+    }
     private final Logger logger = LoggerFactory.getLogger(RestUploadController.class);
 
     //Save the uploaded file to this folder
-    private static String UPLOADED_FOLDER = "/Users/huang/Desktop/123/";
-
+    private static String UPLOADED_FOLDER = "static/upload/";
     //Single file upload
     @PostMapping("/api/upload")
-    // If not @RestController, uncomment this
+    // If not @RestController, uncomment this   
     //@ResponseBody
-    public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile uploadfile) {
-
+    public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile uploadfile, HttpServletRequest req) {
+        String[] address;
         logger.debug("Single file upload!");
-
+        String path=req.getServletContext().getContextPath();
+        System.out.println("地址:"+ ClassUtils.getDefaultClassLoader().getResource("").getPath());
         if (uploadfile.isEmpty()) {
             return new ResponseEntity("please select a file!", HttpStatus.OK);
         }
-
         try {
-
-            saveUploadedFiles(Arrays.asList(uploadfile));
-
-        } catch (IOException e) {
+            address = saveUploadedFiles(Arrays.asList(uploadfile));
+            for (int j = 0; j < address.length; j++) {
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
-
-        return new ResponseEntity("Successfully uploaded - " +uploadfile.getOriginalFilename(), new HttpHeaders(), HttpStatus.OK);
-
+        State state = new State();
+        state.setState("success");
+        state.setUrl(address[0]);
+        return new ResponseEntity(state, new HttpHeaders(), HttpStatus.OK);
     }
 
     // Multiple file upload
     @PostMapping("/api/upload/multi")
     public ResponseEntity<?> uploadFileMulti(
-            @RequestParam("extraField") String extraField,
+//            @RequestParam("extraField") String extraField,
             @RequestParam("files") MultipartFile[] uploadfiles) {
-
         logger.debug("Multiple file upload!");
 
         String uploadedFileName = Arrays.stream(uploadfiles).map(x -> x.getOriginalFilename())
@@ -72,9 +94,11 @@ public class RestUploadController {
 
             saveUploadedFiles(Arrays.asList(uploadfiles));
 
-        } catch (IOException e) {
+        } catch (Exception e) {
+            e.printStackTrace();
             System.out.println(e);
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
         }
 
         return new ResponseEntity("Successfully uploaded - "
@@ -92,7 +116,8 @@ public class RestUploadController {
 
             saveUploadedFiles(Arrays.asList(model.getFiles()));
 
-        } catch (IOException e) {
+        } catch (Exception e) {
+            e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
 
@@ -101,19 +126,40 @@ public class RestUploadController {
     }
 
     //save file
-    private void saveUploadedFiles(List<MultipartFile> files) throws IOException {
-
+    private String[] saveUploadedFiles(List<MultipartFile> files) throws IOException, ParseException {
+            String[] imgAddress = new String[files.size()];
+        int index = 0;
         for (MultipartFile file : files) {
 
             if (file.isEmpty()) {
                 continue; //next pls
             }
-
             byte[] bytes = file.getBytes();
-            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+            Date currentTime = new Date();
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            String date = format.format(currentTime);
+            Path path = Paths.get(ClassUtils.getDefaultClassLoader().getResource("").getPath()+UPLOADED_FOLDER + dateToStamp(date)+".jpg");
             Files.write(path, bytes);
-
+            imgAddress[index] = "/upload/" + dateToStamp(date)+".jpg";
+            index++;
         }
-
+            return imgAddress;
     }
+
+    /**
+     * 时间字符串转时间戳
+     * @param s
+     * @return
+     * @throws ParseException
+     */
+    public static String dateToStamp(String s) throws ParseException{
+        String res;
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date = simpleDateFormat.parse(s);
+        long ts = date.getTime();
+        res = String.valueOf(ts);
+        return res;
+    }
+
+
 }
